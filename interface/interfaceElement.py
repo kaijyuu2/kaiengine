75fbf0aec8fbd90283276cb0cdaa9a3e543a352e
Doctus <kirikayuumura.noir@gmail.com>
2018-10-26 04:42:11 -0400

    "focus key" system

--- interface/interfaceElement.py
+++ interface/interfaceElement.py
@@ -8,31 +8,46 @@ from .screenElement import ScreenElement
 class InterfaceElement(EventIDInterface, ScreenElement, metaclass=_InterfaceElementMeta):
 
     top_level = False
+    inherited_focus_key = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(self)
+        self.addCustomListener(EVENT_INTERFACE_GAIN_FOCUS + self.focus_key, self.focusChanged)
         self._init(*args, **kwargs)
         if self.top_level:
             customEvent(EVENT_INTERFACE_TOP_LEVEL_ELEMENT_CREATED, self)
 
+    @property
+    def focus_key(self):
+        return self.inherited_focus_key or self.id
+
+    def inheritFocusKey(self, key):
+        self.removeCustomListener(EVENT_INTERFACE_GAIN_FOCUS + self.focus_key, self.focusChanged)
+        self.inherited_focus_key = key
+        self.addCustomListener(EVENT_INTERFACE_GAIN_FOCUS + self.focus_key, self.focusChanged)
+
     def _init(self, *args, **kwargs):
         pass
-
-    def gainFocus(self):
-        #clearCustomListeners(INPUT_EVENT_CONFIRM) #maybe..?
-        #clearCustomListeners(INPUT_EVENT_CANCEL)
-        self.addCustomListener(INPUT_EVENT_CONFIRM, self.activate)
-        self.addCustomListener(INPUT_EVENT_CANCEL, self.cancel)
-
-    def loseFocus(self):
-        self.removeCustomListener(INPUT_EVENT_CONFIRM, self.activate)
-        self.removeCustomListener(INPUT_EVENT_CANCEL, self.cancel)
 
     def activate(self):
         self.event(EVENT_INTERFACE_ACTIVATED)
 
     def cancel(self):
         pass
+
+    def gainFocus(self):
+        customEvent(EVENT_INTERFACE_GAIN_FOCUS + self.focus_key, self.id)
+
+    def loseFocus(self):
+        pass
+
+    def focusChanged(self, element_id):
+        if element_id != self.id:
+            self.loseFocus()
+
+    def addChild(self, child_element, *args, **kwargs):
+        child_element.inheritFocusKey(self.focus_key)
+        return super().addChild(child_element, *args, **kwargs)
 
     def destroy(self):
         self.event(EVENT_INTERFACE_DESTROYED)
