054b0b0a53090371d64b7ac64ee846ceccd254ec
Doctus <kirikayuumura.noir@gmail.com>
2019-04-04 23:17:33 -0400

    improved state machine

--- statemachine.py
+++ statemachine.py
@@ -4,40 +4,33 @@
 
 """
 syntax:
-    
-    
-try: smr(VALUE)
-except sm(value1):
-    ...
-except sm(value2):
-    ...
-except:
-    default behavior
-    
-    
 
-note that this will catch errors in the initial sm call if you're doing anything there (ie, smr(0/1) would run the final except block, probably unintentionally). perhaps have an internal try/except block if that's a concern
-also make sure your value is something hashable, or a similar problem will occur. check for TypeError if you want to catch that.
-a final except block is also required else it'll crash instead of doing nothing (just have except: pass).
+
+try: smr(VALUE)
+except sm[value1]:
+    ...
+except sm[value2]:
+    ...
+except SM_DEFAULT:
+    default behavior
+
+
+SM_DEFAULT will only catch unhandled state machine states, not other errors, so you can still catch those in separate blocks
+Also make sure your value is something hashable, or a similar problem will occur. check for TypeError if you want to catch that.
+a final except block is also required else it'll raise an exception instead of doing nothing (just have except: pass).
 
 this is the minimum boilerplate I could come up with
 """
 
-_state_machine_classes = {}
+from collections import defaultdict
 
-def _generateStateMachineClass(val):
-    class new_state_machine_class(Exception):
-        pass
-    return new_state_machine_class
+class SM_DEFAULT(Exception):
+    def __call__(self):
+        class NewStateMachineException(SM_DEFAULT):
+            pass
+        return NewStateMachineException
 
-def sm(val):
-    try:
-        return _state_machine_classes[val]
-    except KeyError:
-        _generateStateMachineClass(val)
-        return _state_machine_classes[val]
-    
-    
+sm = defaultdict(SM_DEFAULT())
+
 def smr(val):
-    raise sm(val)
-    
+    raise sm[val]
